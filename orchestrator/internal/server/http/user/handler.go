edc959a3f3106d876d9d6c48fe35ad9694b5c675
uikola <ugulaev806@yandex.com>
2024-04-21 12:07:55 +0400

    add login handler

---
+++ orchestrator/internal/server/http/user/handler.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"context"
+	"github.com/Uikola/distributedCalculator2/orchestrator/internal/entity"
+)
+
+type userUseCase interface {
+	Create(ctx context.Context, request entity.RegisterRequest) error
+	Login(ctx context.Context, request entity.LoginRequest) (string, error)
+	ParseToken(ctx context.Context, accessToken string) (uint, error)
+	ListOperations(ctx context.Context) (map[string]uint, error)
+	UpdateOperation(ctx context.Context, request entity.UpdateOperationRequest) error
+}
+
+type Handler struct {
+	userUseCase userUseCase
+}
+
+func NewHandler(userUseCase userUseCase) *Handler {
+	return &Handler{
+		userUseCase: userUseCase,
+	}
+}
---
+++ orchestrator/internal/server/http/user/login.go
@@ -0,0 +1,41 @@
+package user
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Uikola/distributedCalculator2/orchestrator/internal/entity"
+	"github.com/Uikola/distributedCalculator2/orchestrator/internal/errorz"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+func (h Handler) Login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var loginRequest entity.LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&loginRequest); err != nil {
+		log.Error().Err(err).Msg("failed to parse request")
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(map[string]string{"reason": "bad json"})
+		return
+	}
+
+	token, err := h.userUseCase.Login(ctx, loginRequest)
+	switch {
+	case errors.Is(err, errorz.ErrUserNotFound):
+		log.Error().Err(err).Msg("user with this login and password not found")
+		w.WriteHeader(http.StatusNotFound)
+		_ = json.NewEncoder(w).Encode(map[string]string{"reason": "user not found"})
+		return
+	case err != nil:
+		log.Error().Err(err).Msg("failed to login user")
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(map[string]string{"reason": "internal error"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	_ = json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
