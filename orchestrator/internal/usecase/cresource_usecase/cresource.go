fdd588d3cd29ff5e0746a24d7c8471c0759f91c3
uikola <ugulaev806@yandex.com>
2024-04-21 12:29:17 +0400

    add use case for computing resource

---
+++ orchestrator/internal/usecase/cresource_usecase/cresource.go
@@ -0,0 +1,37 @@
+package cresource_usecase
+
+import (
+	"context"
+	"github.com/Uikola/distributedCalculator2/orchestrator/internal/entity"
+)
+
+func (uc UseCaseImpl) Create(ctx context.Context, resource entity.CResource) error {
+	return uc.cResourceRepository.Create(ctx, resource)
+}
+
+func (uc UseCaseImpl) Exists(ctx context.Context, name, address string) (bool, error) {
+	return uc.cResourceRepository.Exists(ctx, name, address)
+}
+
+func (uc UseCaseImpl) SetOrchestatorHealth(ctx context.Context, name string, isAlive bool) error {
+	return uc.cResourceRepository.SetOrchestatorHealth(ctx, name, isAlive)
+}
+
+func (uc UseCaseImpl) Delete(ctx context.Context, name string) error {
+	return uc.cResourceRepository.Delete(ctx, name)
+}
+
+func (uc UseCaseImpl) ListCResources(ctx context.Context) (map[string]string, error) {
+	cResources, err := uc.cResourceRepository.ListCResources(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	pairs := make(map[string]string)
+
+	for _, cResource := range cResources {
+		pairs[cResource.Address] = cResource.Expression
+	}
+
+	return pairs, nil
+}
---
+++ orchestrator/internal/usecase/cresource_usecase/usecase.go
@@ -0,0 +1,22 @@
+package cresource_usecase
+
+import (
+	"context"
+	"github.com/Uikola/distributedCalculator2/orchestrator/internal/entity"
+)
+
+type cResourceRepository interface {
+	Create(ctx context.Context, resource entity.CResource) error
+	Exists(ctx context.Context, name, address string) (bool, error)
+	SetOrchestatorHealth(ctx context.Context, name string, isAlive bool) error
+	Delete(ctx context.Context, name string) error
+	ListCResources(ctx context.Context) ([]entity.CResource, error)
+}
+
+type UseCaseImpl struct {
+	cResourceRepository cResourceRepository
+}
+
+func NewUseCaseImpl(cResourceRepository cResourceRepository) *UseCaseImpl {
+	return &UseCaseImpl{cResourceRepository: cResourceRepository}
+}
